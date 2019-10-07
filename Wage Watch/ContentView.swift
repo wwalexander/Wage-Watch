090323ec069dcb118f9ebac9ce17279396e02378
William Alexander <willwalexander@icloud.com>
2019-10-07 13:20:02 +0200

    Initial Commit

---
+++ Wage Watch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wage Watch
+//
+//  Created by William Alexander on 10/7/19.
+//  Copyright © 2019 William Alexander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
